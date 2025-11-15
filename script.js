adfad16231cb2393c80c86574ec65c53a7e76353
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-11-15 18:56:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-// complete the given function
+function palindrome(str) {
+    // keep only alphanumeric characters and convert to lowercase
+    let cleaned = "";
+    for (let ch of str) {
+        if (/[a-zA-Z0-9]/.test(ch)) {
+            cleaned += ch.toLowerCase();
+        }
+    }
 
-function palindrome(str){
-
+    // two-pointer check
+    let l = 0, r = cleaned.length - 1;
+    while (l < r) {
+        if (cleaned[l] !== cleaned[r]) return false;
+        l++;
+        r--;
+    }
+    return true;
 }
-module.exports = palindrome
+
+module.exports = palindrome;
